c3dabe779049e984aafab9f405df33c286674409
parthureddy <parthuredy3@gmail.com>
2025-03-14 18:40:17 -0500

    updated by parthu

--- docker.sh
+++ docker.sh
@@ -11,7 +11,7 @@ lvextend -l +50%FREE /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
-sudo su
+
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
--- docker.tf
+++ docker.tf
@@ -1,5 +1,5 @@
 
-resource "aws_instance" "this" {
+resource "aws_instance" "docker" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
   instance_type          = "t3.micro"
@@ -9,9 +9,28 @@ resource "aws_instance" "this" {
     volume_size = 50  # Set root volume size to 50GB
     volume_type = "gp3"  # Use gp3 for better performance (optional)
   }
-  user_data = file("docker.sh")
-  tags = {
-    Name    = "docker-tf"
+  # user_data = file("docker.sh")
+  # tags = {
+  #   Name    = "docker-tf"
+  # }
+   connection {
+    host = aws_instance.docker.public_ip
+    type = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+  }
+
+  provisioner "file" {
+    source      = "frontend.sh"
+    destination = "/ec2-user/docker.sh"
+  }
+
+  provisioner "remote-exec" {
+    # Bootstrap script called with public_ip of each node in the cluster
+    inline = [
+      "chmod +x /ec2-user/docker.sh",
+      "sudo sh /ec2-user/docker.sh "
+    ]
   }
 }
 
@@ -45,5 +64,5 @@ resource "aws_security_group" "allow_all_docker" {
   }
 }
 output "docker_ip" {
-  value       = aws_instance.this.public_ip
+  value       = aws_instance.docker.public_ip
 }
